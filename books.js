1e46f67b3591348fe3c00271230b892e95a11dec
Nomso <nomsounaogu@gmail.com>
2024-10-01 15:06:51 +0100

    added book images

--- books.js
+++ books.js
@@ -1,27 +1,26 @@
 let myLibrary = [];
 
-//Parameters
-function Book(title, author, pages, read, image)  {
+// Book constructor
+function Book(title, author, pages, read, image) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
   this.image = image;
-  //This initializes a book
 }
 
-//Using prototype allows every instance of the "Book" to have access to this function
+// Prototype method to toggle read status
 Book.prototype.toggleRead = function() {
   this.read = !this.read;
-}
+};
 
-
+// Toggle read status function
 function toggleRead(index) {
   myLibrary[index].toggleRead();
-  render()
+  render();
 }
 
-// image function
+// Image upload function
 document.addEventListener("DOMContentLoaded", function() {
   let profilePic = document.getElementById("profile-pic");
   let inputFile = document.getElementById("input-file");
@@ -30,7 +29,8 @@ document.addEventListener("DOMContentLoaded", function() {
     profilePic.src = URL.createObjectURL(inputFile.files[0]);
   };
 });
-//render function
+
+// Render function
 function render() {
   let libraryEl = document.querySelector("#library");
   libraryEl.innerHTML = "";
@@ -40,14 +40,13 @@ function render() {
     bookEl.setAttribute("class", "book-card");
     bookEl.innerHTML = `
       <div class="card-header">
-        <h3 class="title">${book.title}</h3>
+        <h3 class="book-title">${book.title}</h3>
         <img src="${book.image}" alt="Book Cover" class="book-image">
         <h5 class="author">by ${book.author}</h5>
       </div>
       <div class="card-body">
         <p>${book.pages} pages</p>
         <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
-
         <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
         <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
       </div>
@@ -56,31 +55,33 @@ function render() {
   }
 }
 
-//Remove Button : remove by index
+// Remove book function
 function removeBook(index) {
   myLibrary.splice(index, 1);
   render();
 }
 
-// Updating information in the library
+// Add book function
 function addBookToLibrary() {
   let title = document.querySelector("#title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
   let read = document.getElementById("read").checked;
-  let image = document.getElementById("profile-pic").src; // get image
-  let newBook = new Book(title, author, pages, read, image); // pass image to book constructor
+  let image = document.getElementById("profile-pic").src; // Get image
+  let newBook = new Book(title, author, pages, read, image); // Create new book
   myLibrary.push(newBook);
-  render()
+  render();
 }
 
+// Show form for new book
 let newBookbtn = document.querySelector("#new-book-btn");
-  newBookbtn.addEventListener("click", function() {
-    let newBookForm = document.querySelector("#new-book-form");
-    newBookForm.style.display = "block";
-  })
+newBookbtn.addEventListener("click", function() {
+  let newBookForm = document.querySelector("#new-book-form");
+  newBookForm.style.display = "block";
+});
 
-document.querySelector("#new-book-form").addEventListener("submit", function(event){
+// Handle form submission
+document.querySelector("#new-book-form").addEventListener("submit", function(event) {
   event.preventDefault();
   addBookToLibrary();
-})
+});
